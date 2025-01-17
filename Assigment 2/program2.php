00cc0d3dee60767b585c021bab5d9a72f28ebf84
Raj Yadav <rajyadav9167328@gmail.com>
2025-01-18 00:03:25 +0530

    Assigment 2 php

---
+++ Assigment 2/program2.php
@@ -0,0 +1,51 @@
+<!-- Question 1 -->
+<html>
+    <body bgcolor="#d1dee8">
+        <header>
+            <h2>Q1:</h2>
+        </header>
+    <?php
+    $sum = 0;
+
+// Loop through numbers from 1 to 100
+for ($i = 0; $i <= 100; $i++) {
+    
+    // Check if the number is even
+    if ($i % 2 == 0) {
+
+        // Add the even number to the sum
+        $sum += $i;
+    }
+}
+echo "The sum of all even numbers between 1 and 100 is: " . $sum;
+echo "<hr>";
+?>
+
+
+</body>
+</html>
+
+<!-- //Question 2  -->
+
+<html>
+<body>
+    <header>
+        <h2 style="color:hite">Q2:</h2>
+    </header>
+    <?php
+        
+        //Create an Associative array of cities and their populations
+        $cities = array("Mumbai" =>21673000, 
+                        "Nashik" =>2294000, 
+                        "Pune" => 7345850);
+        $highestpopulation = array_keys($cities, max($cities))[0];
+
+        echo " The Highest population city is : " .$highestpopulation;
+        echo "<hr>";
+
+
+     ?>
+
+
+    </body>
+</html>
